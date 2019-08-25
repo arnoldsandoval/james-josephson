9db9cfe09378fcc524832d746b9b7b8a43c53a41
Arnold Sandoval <arnold@arnie.io>
2019-08-25 11:09:21 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
+/** @jsx jsx */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { jsx, css } from '@emotion/core'
+
+const styles =css`border: none; border:1px solid red; position: relative;`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>James Josephson</h1>
+        <p>Product Designer &ndash; currently working on enterprise products in Menlo Park while getting my masters in Design Impact at Stanford.</p>
       </header>
+
+      <main>
+        <button css={styles}>
+          Password Required
+        </button>
+      </main>
     </div>
   );
 }
